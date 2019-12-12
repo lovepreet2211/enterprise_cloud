df86c8023dad3b887e9bec7a12b9d455aa15f7dc
lovepreet2211 <42843541+lovepreet2211@users.noreply.github.com>
2019-12-12 12:01:24 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+fs.readFile('lovepreet.json', function (err, data) {
+   if (err) throw err;
+   let newData = JSON.parse(data);
+
+   const uri = "mongodb+srv://kaneki:RWgwWHgCEgeaJ5or@cluster0-ktdm9.mongodb.net/test?retryWrites=true&w=majority"
+   MongoClient.connect(uri, {
+      useUnifiedTopology: true
+   }, function (err, client) {
+      if (err) {
+         console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+      }
+      console.log('Connected...');
+      let collection = client.db("project").collection("c0741641_customers");
+      collection.insertMany(newData, function (err, res) {
+         if (err) throw err;
+         console.log('Collections Inserted');
+      })
+      collection.findOne({}, function (err, res) {
+         if(err) throw err;
+         console.log(res);
+        })
+      var oldValue = { 'first_name' : 'Bonny' };
+      var newValue = { $set : { 'first_name': 'Alex' } };
+      collection.updateOne(oldValue, newValue, (err) => {
+         if(err) throw err;
+         console.log("Updated");
+      });
+      client.close();
+   });
+
+})
